a4b60cfed87ad74c04c1d7c6951ae5b2bb683882
Daniel Worthington-Bodart <dan@bodar.com>
2010-04-30 18:26:23 +0100

    Made return chainable

--- src/com/googlecode/yadic/Container.java
+++ src/com/googlecode/yadic/Container.java
@@ -1,10 +1,10 @@
 package com.googlecode.yadic;
 
 public interface Container {
-  <C> void add(Class<C> concrete);
-  <I, C extends I> void add(Class<I> aInterface, Class<C> concrete);
-  <T> void add(Class<T> aClass, Activator<T> activator );
-  <I, C extends I> void decorate(Class<I> i, Class<C> concrete);
+  <C> Container add(Class<C> concrete);
+  <I, C extends I> Container add(Class<I> aInterface, Class<C> concrete);
+  <T> Container add(Class<T> aClass, Activator<T> activator );
+  <I, C extends I> Container decorate(Class<I> i, Class<C> concrete);
   Object resolve(Class aClass);
   <T> T resolveType(Class<T> aClass);
 }
