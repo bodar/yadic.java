4d8e8798aab1af6ccf1c8bd08ca64ece72449008
Daniel Worthington-Bodart <dan@bodar.com>
2010-05-22 09:05:18 +0100

    Changed interface to trait so we can expose convienence method

--- src/com/googlecode/yadic/Container.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.yadic;
-
-public interface Container {
-  <C> Container add(Class<C> concrete);
-  <I, C extends I> Container add(Class<I> aInterface, Class<C> concrete);
-  <T> Container add(Class<T> aClass, Activator<T> activator );
-  <I, C extends I> Container decorate(Class<I> i, Class<C> concrete);
-  <T> T resolveType(Class<T> aClass);
-}
