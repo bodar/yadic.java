42c217d62a42b0d8291601e7ae56a7672d8cccd7
Daniel Worthington-Bodart <dan@bodar.com>
2010-08-20 10:48:46 +0100

    dan, matt: getting activators back out of containers

--- src/com/googlecode/yadic/Container.java
+++ src/com/googlecode/yadic/Container.java
@@ -2,26 +2,28 @@ package com.googlecode.yadic;
 
 import scala.Function0;
 
-public interface Container extends Resolver{
-  <Concrete> Container add(Class<Concrete> concrete);
+public interface Container extends Resolver {
+    <Concrete> Container add(Class<Concrete> concrete);
 
-  <Interface, Concrete extends Interface> Container add(Class<Interface> anInterface, Class<Concrete> concrete);
+    <Interface, Concrete extends Interface> Container add(Class<Interface> anInterface, Class<Concrete> concrete);
 
-  Container addInstance(Object instance);
+    Container addInstance(Object instance);
 
-  <Interface, Concrete extends Interface>Container addInstance(Class<Interface> anInterface, Concrete instance);
+    <Interface, Concrete extends Interface> Container addInstance(Class<Interface> anInterface, Concrete instance);
 
-  <Type> Container addActivator(Class<Type> aClass, Activator<Type> activator);
+    <Type> Container addActivator(Class<Type> aClass, Activator<Type> activator);
 
-  <Type, AnActivator extends Activator<Type>> Container addActivator(Class<Type> aClass, Class<AnActivator> activator);
-    
-  <Type> Container add(Class<Type> aClass, Function0<Type> activator);
+    <Type, AnActivator extends Activator<Type>> Container addActivator(Class<Type> aClass, Class<AnActivator> activator);
 
-  <Interface, Concrete extends Interface> Container decorate(Class<Interface> anInterface, Class<Concrete> concrete);
+    <Type> Container add(Class<Type> aClass, Function0<Type> activator);
 
-  <Type> Activator<Type> remove(Class<Type> aClass);
+    <Interface, Concrete extends Interface> Container decorate(Class<Interface> anInterface, Class<Concrete> concrete);
 
-  <Type> boolean contains(Class<Type> aClass);
+    <Type> Activator<Type> remove(Class<Type> aClass);
 
-  <Type> Type resolveType(Class<Type> aClass);
+    <Type> boolean contains(Class<Type> aClass);
+
+    <Type> Type resolveType(Class<Type> aClass);
+
+    <Type> Activator<Type> getActivator(Class<Type> aClass);
 }
