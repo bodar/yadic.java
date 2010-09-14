951cb8d99c7c816c0f34909edcfb58292cf2dac2
Daniel Worthington-Bodart <dan@bodar.com>
2010-09-14 11:30:37 +0100

    dan, stu: Added support for capturing nested ContainerExceptions

--- src/com/googlecode/yadic/ContainerException.scala
+++ src/com/googlecode/yadic/ContainerException.scala
@@ -2,8 +2,8 @@ package com.googlecode.yadic
 
 import java.util.Arrays.asList
 
-class ContainerException(message:String, causes:List[ContainerException]) extends RuntimeException(message, causes.last){
-  def this(message:String) = this(message, List(null) )
+class ContainerException(message:String, causes:List[ContainerException]) extends RuntimeException(message, if(causes.isEmpty) null else causes.last){
+  def this(message:String) = this(message, List() )
 
   def getCauses:java.util.List[ContainerException] = asList(causes.toArray: _*)
 }
