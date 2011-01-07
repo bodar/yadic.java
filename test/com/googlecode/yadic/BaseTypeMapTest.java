cd546a6bea88a5b74840fadbcab204b0c5c3e7bf
Daniel Worthington-Bodart <dan@bodar.com>
2011-01-07 16:44:49 +0000

    Added gurads against object resolving, fixed option activation and added support composite resolvers

---
+++ test/com/googlecode/yadic/BaseTypeMapTest.java
@@ -0,0 +1,18 @@
+package com.googlecode.yadic;
+
+import com.googlecode.yadic.resolvers.MissingResolver;
+import org.junit.Test;
+
+public class BaseTypeMapTest {
+    @Test (expected = ContainerException.class)
+    public void canNeverAddAnObjectType() throws Exception {
+        TypeMap typeMap = new BaseTypeMap(new MissingResolver());
+        typeMap.add(Object.class, Object.class);
+    }
+
+    @Test (expected = ContainerException.class)
+    public void canNeverResolveAnObjectType() throws Exception {
+        TypeMap typeMap = new BaseTypeMap(new MissingResolver());
+        typeMap.resolve(Object.class);
+    }
+}
