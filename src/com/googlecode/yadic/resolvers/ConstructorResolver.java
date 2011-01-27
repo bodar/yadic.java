7b43c14b0aa9ccf0ed726ccf6ab4e7130b82f8f4
Daniel Worthington-Bodart <dan@bodar.com>
2011-01-27 14:43:45 +0000

    Prevent resolving via a self referencing constructor

--- src/com/googlecode/yadic/resolvers/ConstructorResolver.java
+++ src/com/googlecode/yadic/resolvers/ConstructorResolver.java
@@ -9,12 +9,17 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.googlecode.totallylazy.Arrays.exists;
 import static com.googlecode.totallylazy.Callables.cast;
 import static com.googlecode.totallylazy.Callables.descending;
+import static com.googlecode.totallylazy.Constructors.genericParameterTypes;
 import static com.googlecode.totallylazy.Option.none;
 import static com.googlecode.totallylazy.Option.some;
+import static com.googlecode.totallylazy.Predicates.not;
+import static com.googlecode.totallylazy.Predicates.where;
 import static com.googlecode.totallylazy.Sequences.sequence;
 import static com.googlecode.yadic.generics.TypeConverter.convertParametersToInstances;
+import static com.googlecode.yadic.generics.Types.matches;
 import static com.googlecode.yadic.resolvers.Resolvers.asCallable1;
 import static com.googlecode.yadic.generics.TypeConverter.typeConverter;
 import static com.googlecode.yadic.generics.Types.classOf;
@@ -28,7 +33,9 @@ public class ConstructorResolver<T> implements Resolver<T> {
 
     public T resolve(Type type) throws Exception {
         Class<T> concrete = classOf(type);
-        Sequence<Constructor<?>> constructors = sequence(concrete.getConstructors()).sortBy(descending(numberOfParamters()));
+        Sequence<Constructor<?>> constructors = sequence(concrete.getConstructors()).
+                filter(where(genericParameterTypes(), not(exists(matches(type))))).
+                sortBy(descending(numberOfParamters()));
         if (constructors.isEmpty()) {
             throw new ContainerException(concrete.getName() + " does not have a public constructor");
         }
