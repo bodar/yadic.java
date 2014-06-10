030a75d9a4bf0cc946f76d7fea202f17a7f938c4
Daniel Worthington-Bodart <dan@bodar.com>
2014-06-10 09:22:09 +0100

    Minor cleanup

--- src/com/googlecode/yadic/collections/Activator.java
+++ src/com/googlecode/yadic/collections/Activator.java
@@ -11,17 +11,28 @@ import static com.googlecode.totallylazy.Callables.when;
 import static com.googlecode.totallylazy.Functions.constant;
 import static com.googlecode.totallylazy.Sequences.sequence;
 
-public class Activator<T> implements Function<Iterable<Activator<?>>, T>, Predicate<Type>, AutoCloseable {
+public class Activator<T> implements Function<Iterable<Activator<?>>, T>, Predicate<Type>, AutoCloseable, Value<Sequence<T>> {
     private final Function<? super Iterable<Activator<?>>, T> constructor;
     private final Block<? super T> destructor;
     private final Predicate<? super Type> matcher;
     private final LazyFunction<Iterable<Activator<?>>, T> instances;
 
-    public Activator(Function<? super Iterable<Activator<?>>, T> constructor, Block<? super T> destructor, Predicate<? super Type> matcher) {
+    private Activator(Function<? super Iterable<Activator<?>>, T> constructor, Block<? super T> destructor, Predicate<? super Type> matcher) {
         this.matcher = matcher;
         this.constructor = constructor;
         this.destructor = destructor;
         instances = LazyFunction.lazy(constructor);
+    }
+
+    public static <T> Activator<T> activator(Function<? super Iterable<Activator<?>>, T> constructor, Block<? super T> destructor, Predicate<? super Type> matcher) {
+        return new Activator<T>(constructor, destructor, matcher);
+    }
+
+    public static <T> Activator<T> activator(Class<T> aClass) {
+        return activator(
+                list -> Activators.create(aClass, list),
+                Activators.destructor(aClass),
+                type -> Types.matches(aClass, type));
     }
 
     @Override
@@ -31,19 +42,17 @@ public class Activator<T> implements Function<Iterable<Activator<?>>, T>, Predic
 
     @Override
     public void close() throws Exception {
-        instances.value().each((Function<? super T, Void>) destructor);
+        value().each((Function<? super T, Void>) destructor);
+    }
+
+    @Override
+    public Sequence<T> value() {
+        return instances.value();
     }
 
     @Override
     public boolean matches(Type other) {
         return matcher.matches(other);
-    }
-
-    public static <T> Activator<T> activator(Class<T> aClass) {
-        return new Activator<T>(
-                list -> Activators.create(aClass, list),
-                Activators.destructor(aClass),
-                type -> Types.matches(aClass, type));
     }
 
     @SafeVarargs
@@ -64,20 +73,20 @@ public class Activator<T> implements Function<Iterable<Activator<?>>, T>, Predic
     }
 
     public Activator<T> constructor(Function<? super Iterable<Activator<?>>, T> constructor) {
-        return new Activator<>(constructor, destructor, matcher);
+        return activator(constructor, destructor, matcher);
     }
 
     public Activator<T> destructor(Block<T> destructor) {
-        return new Activator<>(constructor, destructor, matcher);
+        return activator(constructor, destructor, matcher);
     }
 
     public Activator<T> matcher(Predicate<? super Type> predicate) {
-        return new Activator<>(constructor, destructor, predicate);
+        return activator(constructor, destructor, predicate);
     }
 
     public PersistentList<Activator<?>> decorate(Type typeToDecorate, PersistentList<Activator<?>> activators) {
         return activators.map(when(a -> a.matches(typeToDecorate),
-                original -> new Activator<>(
+                original -> activator(
                         list -> constructor.call(Sequences.cons(original, list)),
                         destructor,
                         type -> Types.matches(typeToDecorate, type))
