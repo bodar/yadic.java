b15dc811ba5df525d655f28984722d4aa33e4deb
Daniel Worthington-Bodart <dan@bodar.com>
2014-06-17 09:31:41 +0100

    Added concrete method

--- test/com/googlecode/yadic/collections/ListResolverTest.java
+++ test/com/googlecode/yadic/collections/ListResolverTest.java
@@ -50,12 +50,12 @@ public class ListResolverTest {
 
     @Test(expected = ContainerException.class)
     public void throwsExceptionIfNotFound() throws Exception {
-        listResolver(list()).resolve(SomeClosableClass.class);
+        listResolver().resolve(SomeClosableClass.class);
     }
 
     @Test
     public void supportsRegisteringAgainstInterfaces() throws Exception {
-        ListResolver resolver = listResolver(list(concreate(RootNode.class).interfaces(Node.class)));
+        ListResolver resolver = listResolver(concreate(RootNode.class).interfaces(Node.class));
         Node instance = (Node) resolver.resolve(Node.class);
         assertThat(instance, instanceOf(RootNode.class));
     }
@@ -71,28 +71,28 @@ public class ListResolverTest {
     @Test
     public void supportsCustomConstruction() throws Exception {
         RootNode instance = new RootNode();
-        ListResolver resolver = listResolver(list(concreate(RootNode.class).constructor(list -> instance)));
+        ListResolver resolver = listResolver(concreate(RootNode.class).constructor(list -> instance));
         assertThat(resolver.resolve(RootNode.class), sameInstance(instance));
     }
 
     @Test
     public void supportsInstance() throws Exception {
         RootNode rootNode = new RootNode();
-        ListResolver resolver = listResolver(list(instance(rootNode)));
+        ListResolver resolver = listResolver(instance(rootNode));
         assertThat(resolver.resolve(RootNode.class), sameInstance(rootNode));
     }
 
     @Test
     public void supportsInstanceWithSpecificInterface() throws Exception {
         RootNode rootNode = new RootNode();
-        ListResolver resolver = listResolver(list(instance(rootNode).interfaces(Node.class)));
+        ListResolver resolver = listResolver(instance(rootNode).interfaces(Node.class));
         assertThat(resolver.resolve(Node.class), sameInstance(rootNode));
     }
 
     @Test
     public void supportsCustomDestruction() throws Exception {
         AtomicBoolean called = new AtomicBoolean(false);
-        ListResolver resolver = listResolver(list(concreate(SomeClosableClass.class).destructor(instance -> called.set(true))));
+        ListResolver resolver = listResolver(concreate(SomeClosableClass.class).destructor(instance -> called.set(true)));
         SomeClosableClass instance = (SomeClosableClass) resolver.resolve(SomeClosableClass.class);
         assertThat(called.get(), is(false));
         assertThat(instance.closed, is(false));
@@ -103,12 +103,12 @@ public class ListResolverTest {
 
     @Test
     public void supportsGenerics() throws Exception {
-        Resolver resolver = listResolver(list(
+        Resolver resolver = listResolver(
                 instance("bob"),
                 instance(1),
                 concreate(GenericType.class).types(new TypeFor<GenericType<Integer>>() { }),
                 concreate(UsesGenericType.class)
-        ));
+        );
         UsesGenericType genericType = (UsesGenericType) resolver.resolve(UsesGenericType.class);
         assertThat(genericType.instance().instance(), is(1));
     }
